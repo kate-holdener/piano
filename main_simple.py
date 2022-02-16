5d0c3c4432e68526525a0425585429953b7a3bf4
danmao1 <90938757+danmao1@users.noreply.github.com>
2022-02-16 09:31:51 -0600

    removed code duplication

--- main_simple.py
+++ main_simple.py
@@ -1,6 +1,6 @@
 from rainbow_controller import RainbowController
 from piano_lights import LightsFromPiano
 
-lights_controller = SimpleController(5, (244,0,0), (0,0,255))
+lights_controller = RainbowController(5, (244,0,0), (0,0,255))
 piano_input = LightsFromPiano()
 piano_input.run(lights_controller)
