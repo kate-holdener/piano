7646683cb7a0db9247456d182a29cbc5b6a86ae1
Kate Holdener <kate.holdener@slu.edu>
2022-02-17 15:25:17 -0600

    fixed import issues

--- main_simple.py
+++ main_simple.py
@@ -1,6 +1,6 @@
-import light_controller.simple_controller.SimpleController
+from light_controller import simple_controller
 from piano_lights import LightsFromPiano
 
-lights_controller = SimpleController(5, (244,0,0), (0,0,255))
+lights_controller = simple_controller.SimpleController(5, (244,0,0), (0,0,255))
 piano_input = LightsFromPiano()
 piano_input.run(lights_controller)
