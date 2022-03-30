19777a4872a3f193300b39d11675726706395bcc
Kate Holdener <kate.holdener@slu.edu>
2022-03-29 22:32:46 -0500

    added a test for simple controller

---
+++ tests/light_controllers/simple_controller_test.py
@@ -0,0 +1,21 @@
+from entity.midi_event import Event
+from light_controllers.simple_controller import SimpleController
+
+class Api:
+    def __init__(self, num_lights):
+        self.lights = [(-1,-1,-1)] * num_lights
+
+    def get_num_lights(self):
+        return len(self.lights)
+
+    def set_light(self, index, color):
+        self.lights[index] = color
+
+def test_first_key_down():
+    api = Api(10)
+    down_event = Event(144, 45, 20, 1)
+    simple_controller = SimpleController(api, (5,5,5), (1,1,1))
+    simple_controller.process_event(down_event)
+    assert api.lights[0] == (5,5,5)
+    for color in api.lights[1:]:
+        assert color == (-1,-1,-1)
--- tests/use_cases/event_queue_runner_test.py
+++ tests/use_cases/event_queue_runner_test.py
@@ -4,7 +4,7 @@ from use_cases.event_queue_runner import EventQueueRunner
 class Controller:
     def __init__(self):
         self.event_count = 0
-    def process_event(self, event):
+    def process_event(self, event):  # pylint: disable-unused-argument
         self.event_count+=1
 
 def test_one_event():
@@ -15,4 +15,3 @@ def test_one_event():
     event_queue_runner.drain_queue()
     assert test_controller.event_count == 1
     assert event_queue.empty()
-
