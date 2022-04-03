4dd2f82c7dc94ebc3635226e39ca0185ce7fc97d
Kate Holdener <kate.holdener@slu.edu>
2022-04-03 13:59:01 -0500

    fixed pylint issue

--- light_controllers/simple_controller.py
+++ light_controllers/simple_controller.py
@@ -2,7 +2,7 @@ from .light_controller import LightController
 class SimpleController(LightController):
     """
     Changes lights on each key down event such that the next
-    light gets set to the ON color and the previous light that 
+    light gets set to the ON color and the previous light that
     was set to the ON color is set to the OFF color. When the
     strand runs out of lights, the *next* light is the first light
     in the strand, simulating a circular light strand.
